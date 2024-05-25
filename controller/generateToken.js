9dfe4745936c7b8845cd1283914af6419dbf3fd4
arnab@selegic.com <arnab@selegic.com>
2024-05-25 16:51:51 +0530

    json web token -- implementation

--- controller/generateToken.js
+++ controller/generateToken.js
@@ -1,12 +1,12 @@
+// json web token implementation
+var jwt = require("jsonwebtoken");
 
-var jwt = require('jsonwebtoken');
-
-module.exports.authenticate = async(req, res) => {
-    const userId = req.body.uuid;
-    console.log('userName : ', userId);
-    const profile = {uuid : userId}
-    console.log('profile : ', profile);
-    const token = jwt.sign(profile, 'secret_key', { expiresIn: '1h' });
-    //res.locals.token = token;
-    res.json({token : token});
+module.exports.authenticate = async (req, res) => {
+  const userId = req.body.uuid;
+  console.log("userName : ", userId);
+  const profile = { uuid: userId };
+  console.log("profile : ", profile);
+  const token = jwt.sign(profile, "secret_key", { expiresIn: "1h" });
+  //res.locals.token = token;
+  res.json({ token: token });
 };
